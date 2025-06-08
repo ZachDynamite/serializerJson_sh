7feda91498cf0eadb660de0b0e97da403a6280c1
ZachDynamite <38150084+ZachDynamite@users.noreply.github.com>
2025-06-08 14:23:21 -0500

    Update serializerJson.sh

--- serializerJson.sh
+++ serializerJson.sh
@@ -1,21 +1,15 @@
 #!/usr/bin/env bash
-
 set -euo pipefail
-
 #CloudTrail directory#
 read -rp "Enter the full or relative path to the directory: " dirMasterJson
-
 #Validate the input path#
 if [[ ! -d "$dirMasterJson" ]]; then
   echo "[ERROR] The directory '$cloudtrail_dir' does not exist or is not accessible."
   exit 1
 fi
-
 #Output file#
 outfile="T.ndjson"
 > "$outfile"
-
 #Command#
 find "$dirMasterJson" -type f -name "*.json" -exec jq -c '.Records[]' {} \; > "$outfile"
-
 echo "[*]: Operation complete: $outfile saved"
